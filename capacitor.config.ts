9aefaa3c7fe8edb85ed2df444f02002a2667d819
toni powell <tonipow3ll@gmail.com>
2022-02-28 18:39:48 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DTR-FE',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
